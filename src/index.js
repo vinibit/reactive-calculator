14df3726391bc531367e07e5df0a4f824ee70f4c
Vinicius Martins <vinibitmartins@gmail.com>
2024-11-29 19:32:08 -0300

    Cria e define estilos para o componente Calculator

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import Calculator from './main/Calculator'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<>
+			<h1>Calculator</h1>
+			<Calculator />		
+		</>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
